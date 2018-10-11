0a514ff78cc992ec9a7b6f2b655475a9e772dba4
emily <limeyb7@gmail.com>
2018-10-11 22:36:22 +0100

    🐣  tidy up file

--- pets-extended.js
+++ pets-extended.js
@@ -1,81 +1,73 @@
+if (!console.table) require("console.table");
+
 const crossfilter = require("crossfilter");
-require("console.table");
-
-const data = [
-  {
-    name: "Emily",
-    gender: "F",
-    pets: []
-  },
-  {
-    name: "Mike",
-    gender: "M",
-    pets: []
-  },
-  {
-    name: "Will",
-    gender: "M",
-    pets: [{ type: "cat", age: 7, name: "Joanne" }]
-  },
-  {
-    name: "Lewi",
-    gender: "M",
-    pets: [{ type: "cat", age: 1, name: "Sonny" }]
-  }
-];
-
-const identity = a => a;
+const data = require("./people.json");
 
 const cx = crossfilter(data);
-
 const d0 = cx.dimension(() => true);
 const d1 = cx.dimension(d => d.name);
 const d2 = cx.dimension(d => d.pets.length);
 const d3 = cx.dimension(d => d.gender);
 
-(function() {
-  const extendPets = obj => ({
-    ...obj,
-    pets: obj.pets.length
-      ? `${obj.pets.length} ${obj.pets[0].type}`
-      : "No pets :("
-  });
-  console.log("[33m%s[0m", "🍉 top 2 by number of pets");
-  console.table(d2.top(2).map(extendPets));
-  console.log("-------------------");
-  console.log("[35m%s[0m", "☕️ bottom 2 by number of pets");
-  console.table(d2.bottom(2).map(extendPets));
+const addPets = (acc, { pets }) => ({
+  ...acc,
+  total: acc.total + pets.length,
+  count: acc.count + 1
+});
 
-  // filtering
-  d3.filter(gender => gender === "F");
+const removePets = (acc, { pets }) => ({
+  ...acc,
+  total: acc.total - pets.length,
+  count: acc.count - 1
+});
+
+const initCountPets = () => ({
+  total: 0,
+  count: 0
+});
+
+const printPetDetail = person => ({
+  ...person,
+  pets: person.pets.length
+    ? `${person.pets.length} ${person.pets[0].type}`
+    : "No pets :("
+});
+
+const filterFemale = dimension => {
+  dimension.filter(gender => gender === "F");
+  let result = dimension.top(2);
+  dimension.filterAll();
+  return result;
+};
+
+const printAll = ({ top, bottom, female, average }) => {
+  // top pet owners
+  console.log("[33m%s[0m", "🍉   top 2 by number of pets");
+  console.table(top.map(printPetDetail));
+  console.log("-------------------");
+
+  // bottom pet owners
+  console.log("[35m%s[0m", "☕️    bottom 2 by number of pets");
+  console.table(bottom.map(printPetDetail));
+  console.log("-------------------");
+
+  // female
   console.log("[33m%s[0m", "🥦   top female pet owners");
-  console.table(d2.top(1).map(extendPets));
-  d3.filterAll();
-  console.log("-------------------");
+  console.table(female.map(printPetDetail));
 
-  // aggregations
-  const init = () => ({
-    total: 0,
-    count: 0
-  });
-
-  const add = (a, { pets }) => ({
-    ...a,
-    total: a.total + pets.length,
-    count: a.count + 1
-  });
-
-  const remove = (a, { pets }) => ({
-    ...a,
-    total: a.total - pets.length,
-    count: a.count - 1
-  });
-  console.log("[33m%s[0m", "🦁 average number of pets");
+  // average number of pets
+  console.log("[33m%s[0m", "🦁    average number of pets by gender");
   console.table(
-    d3
-      .group()
-      .reduce(add, remove, init)
-      .all()
-      .map(x => ({ gender: x.key, average: x.value.total / x.value.count }))
+    average.map(x => ({
+      gender: x.key,
+      average: x.value.total / x.value.count
+    }))
   );
-})();
+};
+
+printAll({
+  top: d2.top(2),
+  bottom: d2.bottom(2),
+  female: filterFemale(d3),
+  average: d3.group().reduce(addPets, removePets, initCountPets).all()
+});
